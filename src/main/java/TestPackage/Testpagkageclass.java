c396506e6b8f7e22b13c9ba2ab4a2bb94532ffda
Hermano <herekd-7@student.ltu.se>
2021-04-21 12:34:00 +0200

    Initial commit

---
+++ src/main/java/TestPackage/Testpagkageclass.java
@@ -0,0 +1,17 @@
+package TestPackage;
+
+public class Testpagkageclass {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 15;
+
+        if (a > b) {
+            System.out.println("a är större än b");
+            } else; {
+            System.out.println("matten suger");
+        }
+    }
+}
+
+
+
